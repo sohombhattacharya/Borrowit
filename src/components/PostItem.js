d0df3a66ea8d58f451c9c8882f4a7befb645b3d1
sohombhattacharya <sohom95@yahoo.com>
2018-02-05 22:42:19 -0500

    created multiple image upload and deletion for Sell It page

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -11,13 +11,13 @@ import {auth} from "./Constants.js"
 class PostItem extends React.Component {
     
     uploadButton(){
-cloudinary.openUploadWidget({upload_preset: 'fjpbxars', cloud_name: "dycjqocml", cropping: 'server', folder: 'user_photos', sources: [ 'local', 'url', 'dropbox', 'facebook', 'instagram']}, 
+cloudinary.openUploadWidget({upload_preset: 'fjpbxars', cloud_name: "dycjqocml", thumbnails: '.upload_multiple_images_holder', form: '.upload_multiple_images_holder', thumbnail_transformation: [ {width: 200, height: 200, crop: 'fit'} ], multiple: true, cropping: 'server', theme: "minimal", folder: 'user_photos', sources: [ 'local', 'url', 'dropbox', 'facebook', 'instagram']}, 
   function(error, result) {console.log(error, result)});
     
     
     }
     render() {
-        return (<div><button type="button" class="btn btn-danger" onClick={this.uploadButton}>Upload</button></div>); 
+        return (<div><div class="upload_multiple_images_holder"><button type="button" class="btn btn-danger" onClick={this.uploadButton}>Upload</button></div></div>); 
   }
 }
 
